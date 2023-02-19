4dc1f05b71cae2337b8e9b26bbf92780c4ae1df0
TheLeerName <lesha.nechaev.05@mail.ru>
2023-02-20 02:03:21 +0700

    i hate final

--- source/PlayerSettings.hx
+++ source/PlayerSettings.hx
@@ -7,7 +7,6 @@ import flixel.util.FlxSignal;
 
 // import ui.DeviceManager;
 // import props.Player;
-@:build(macros.SetCallbacksBuilding.build())
 class PlayerSettings
 {
 	static public var numPlayers(default, null) = 0;
